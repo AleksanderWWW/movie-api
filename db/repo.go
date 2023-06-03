4cbc2b5cb9f816e287c5295cc73a9574bc1b2d39
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-06-03 14:04:12 +0200

    improve repo

--- db/repo.go
+++ db/repo.go
@@ -6,7 +6,7 @@ import "movie-api/internal/movie"
 type Repo interface {
 	getAllMovies() []movie.Movie
 	getMovieByID(id int) movie.Movie
-	createMovie(movie.Movie)
-	UpdateMovie(movie.Movie)
-	DeleteMovie(id int)
+	createMovie(movie.Movie) error
+	UpdateMovie(movie.Movie) error
+	DeleteMovie(id int) error
 }
--- db/repo_sqlite.go
+++ db/repo_sqlite.go
@@ -15,11 +15,11 @@ func (s *SqliteRepo) getAllMovies() []movie.Movie {
 
 	res, err := s.conn.Query("SELECT * FROM MOVIES")
 
-	if err != nil {
-		return []movie.Movie{}
-	}
-
 	var results []movie.Movie
+
+	if err != nil {
+		return results
+	}
 
 	for res.Next() {
 		var singleMovie movie.Movie
@@ -34,3 +34,23 @@ func (s *SqliteRepo) getAllMovies() []movie.Movie {
 	return results
 
 }
+
+func (s *SqliteRepo) getMovieByID(id int) movie.Movie {
+	var singleMovie movie.Movie
+
+	res, err := s.conn.Query("SELECT * FROM MOVIES WHERE ID = " + string(rune(id)))
+
+	if err != nil {
+		return singleMovie
+	}
+
+	_ = res.Scan(&singleMovie.ID, &singleMovie.Title, &singleMovie.Director, &singleMovie.Year)
+
+	return singleMovie
+}
+
+func (s *SqliteRepo) createMovie(movie movie.Movie) error
+
+func (s *SqliteRepo) updateMovie(movie movie.Movie) error
+
+func (s *SqliteRepo) DeleteMovie(id int) error

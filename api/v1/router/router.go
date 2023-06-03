81733f3d4a084ca39595461b497e118a7de1a097
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-06-03 13:36:37 +0200

    move to chi router

--- api/v1/router/router.go
+++ api/v1/router/router.go
@@ -1,1 +1,26 @@
 package router
+
+import (
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+
+)
+
+func Initialize() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(
+		middleware.Logger,
+		middleware.RedirectSlashes, 
+		middleware.Recoverer, //middleware to recover from panics
+		middleware.Heartbeat("/health"), //for heartbeat process such as Kubernetes liveprobeness
+		
+	)
+
+	//Sets context for all requests
+	router.Use(middleware.Timeout(30 * time.Second))
+
+	return router
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module movie-api
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.8
+)

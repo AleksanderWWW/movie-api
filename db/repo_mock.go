dcb820efa49f95a1c24509cdf72eb81d9c629967
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-06-08 19:31:07 +0200

    improve repo mock

--- db/repo_mock.go
+++ db/repo_mock.go
@@ -1,26 +1,28 @@
 package db
 
 import (
-	"fmt"
+	"errors"
 	"movie-api/internal/movie"
 )
+
+var storage = []movie.Movie{
+	{ID: 1, Title: "some title 1", Director: "some director 1", Year: 2021},
+	{ID: 2, Title: "some title 2", Director: "some director 2", Year: 2022},
+}
 
 type MockRepo struct{}
 
 func (m *MockRepo) GetAllMovies() []movie.Movie {
-	return []movie.Movie{
-		{ID: 1, Title: "some title 1", Director: "some director 1", Year: 2021},
-		{ID: 2, Title: "some title 2", Director: "some director 2", Year: 2022},
-	}
+	return storage
 }
 
 func (m *MockRepo) GetMovieByID(id int) movie.Movie {
-	return movie.Movie{
-		ID:       id,
-		Title:    fmt.Sprintf("some title %d", id),
-		Director: fmt.Sprintf("some director %d", id),
-		Year:     2020 + id,
+	for _, mv := range storage {
+		if mv.ID == id {
+			return mv
+		}
 	}
+	return movie.Movie{}
 }
 
 func (m *MockRepo) CreateMovie(movie movie.Movie) error {
@@ -28,7 +30,12 @@ func (m *MockRepo) CreateMovie(movie movie.Movie) error {
 }
 
 func (m *MockRepo) UpdateMovie(movie movie.Movie) error {
-	return nil
+	for _, mv := range storage {
+		if mv.ID == movie.ID {
+			return nil
+		}
+	}
+	return errors.New("Error updating movie.")
 }
 
 func (m *MockRepo) DeleteMovie(id int) error {
